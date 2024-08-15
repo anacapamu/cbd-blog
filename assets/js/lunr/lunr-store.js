19da234f973413915231d7600242946aa9311181
anacapamu <anacapamu@users.noreply.github.com>
2024-08-15 00:00:25 +0000

    deploy: 2dd8ebcf90f5d7397b4f3a8f62c91c3c5943252d

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Little Crab's Adventure",
+        "excerpt":"I have been reading cloth books to my kiddo, who loves playing with the flaps. With her attention span being less than 5 minutes, these short books (usually one sentence per page and around five pages long) are perfect. Since the books we appreciatively got from other moms are in...","categories": ["bookcabulary","mandarin chinese"],
+        "tags": ["sea life"],
+        "url": "/little-crabs-adventure/",
+        "teaser": null
+      },{
+        "title": "Things That Go",
+        "excerpt":"My daughter has been obsessed with transportation books like Things That Go ever since our trip to Canada. During our travels, we were hopping on trains to and from airports, catching buses to sightseeing spots, embarking on boats to islands, and boarding planes to different countries. It was a whirlwind...","categories": ["bookcabulary","mandarin chinese"],
+        "tags": ["transportation"],
+        "url": "/things-that-go/",
+        "teaser": null
+      }]
